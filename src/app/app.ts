7a38dc3771f6feb5f10ebbb29998fab560a02d80
rosa0922 <rohit.savalagi@netcracker.com>
2026-08-26 20:25:39 +0530

    Implement task management features with form handling and styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Tasks } from "./tasks/tasks";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Tasks],
+  imports: [Tasks],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
